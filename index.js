d52da5933c66b44bc8fad0ffe0e6121e5f999817
Studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-04 23:41:40 +1100

    phase-0-pac-3-arithmetic-lab

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x,y){
+return x+y
+}
+
+function subtract(x,y){
+return x-y
+}
+
+function multiply(x,y){
+return x*y
+}
+
+function divide(x,y){
+return x/y
+}
+
+function increment(x){
+return x+1
+}
+
+function decrement(x){
+return x-1
+}
+
+function makeInt(a){
+return parseInt(a,10)
+}
+
+function preserveDecimal(x){
+return parseFloat(x,2)
+}
+
